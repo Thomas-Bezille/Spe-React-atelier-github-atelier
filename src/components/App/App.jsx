e3c69ba3a0b5a7bd674a2fb28fbc69a6bcfb57f1
thomas <bezille.thomas@proton.me>
2024-04-24 13:49:27 +0200

    Création du champs contrôlé

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,23 @@
+import { useState } from 'react';
+
 import Header from '../Header/Header';
 import Message from '../Message/Message';
 import ReposResults from '../ReposResults/ReposResults';
-
-import './App.scss';
 import data from '../../data/repos';
 
+import './App.scss';
+
 function App() {
+  // Variable de state pour les repos -> default {}
+  const [repos, setRepos] = useState(data);
+  // Variable de state pour le contrôle de l'input -> default ''
+  const [inputSearch, setInputSearch] = useState('');
+
   return (
     <div className="App">
-      <Header />
-      <Message count={data.total_count} />
-      <ReposResults data={data} />
+      <Header searchValue={inputSearch} setSearchValue={setInputSearch} />
+      <Message count={repos.total_count} />
+      <ReposResults data={repos} />
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,32 @@
+import PropTypes from 'prop-types';
 import { Icon, Input } from 'semantic-ui-react';
 
 import './Header.scss';
 import logo from '../../assets/img/logo-github.png';
 
-const Header = () => {
+const Header = ({ searchValue, setSearchValue }) => {
   return (
     <header className="header">
       <img src={logo} alt="logo" className="logo" />
       <form className="form">
         <Input icon placeholder="Rechercher..." iconPosition="left">
           <Icon name="search" />
-          <input />
+          <input
+            type="text"
+            value={searchValue}
+            onChange={(event) => {
+              setSearchValue(event.target.value);
+            }}
+          />
         </Input>
       </form>
     </header>
   );
+};
+
+Header.propTypes = {
+  searchValue: PropTypes.string.isRequired,
+  setSearchValue: PropTypes.func.isRequired,
 };
 
 export default Header;
